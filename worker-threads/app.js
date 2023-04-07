11360c826107a035dc941e76b3d6870b1bc729d7
Asaad Saad <asaad@outlook.com>
2023-04-07 09:57:15 -0500

    Delete worker-threads directory

--- worker-threads/app.js
+++
@@ -1,4 +0,0 @@
-const { Worker } = require('worker_threads');
-
-const worker = new Worker('./worker.js', { workerData: { name: 'Asaad Saad' } });
-worker.on('message', (msg) => { console.log(msg); });
--- worker-threads/worker.js
+++
@@ -1,4 +0,0 @@
-const { workerData, parentPort } = require('worker_threads');
-
-const data = { ...workerData, course: 'CS572' }
-parentPort.postMessage({ data });
